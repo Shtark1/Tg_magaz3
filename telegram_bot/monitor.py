e7b5b994310465bed1d060691edcce97549b4bae
sergg <95623529+Shtark1@users.noreply.github.com>
2023-10-16 00:21:09 +0300

    5.1

--- telegram_bot/monitor.py
+++ telegram_bot/monitor.py
@@ -4,7 +4,6 @@ if __name__ == '__main__':
     while True:
         try:
             subprocess.run(["/home/str/Tg_magaz3/.venv/bin/python", "/home/str/Tg_magaz3/start_all_bot.py"])
-            # subprocess.run(["python", "start_all_bot.py"])
         except KeyboardInterrupt:
             break
         except Exception as e:
